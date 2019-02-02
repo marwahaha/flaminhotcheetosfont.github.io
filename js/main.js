ee19b080dc71bb627c4872efde91cfb458193965
ericadu <erica.c.du@gmail.com>
2019-02-01 22:38:15 -0500

    Rename punctuation.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 var alphabet = {
-  '.': '_period',
-  ',': '_comma',
-  '?': '_q',
-  '!': '_e'
+  '.': 'period',
+  ',': 'comma',
+  '?': 'question',
+  '!': 'excl'
 }
 
 window.onload = function hotCheeto() {
@@ -16,6 +16,24 @@ function handleClear() {
     c.removeChild(c.lastChild);
   }
 }
+
+// (() => {
+//   'use strict';
+//   // Page is loaded
+//   const objects = document.getElementsByClassName('asyncImage');
+//   Array.from(objects).map((item) => {
+//     // Start loading image
+//     const img = new Image();
+//     img.src = item.dataset.src;
+//     // Once image is loaded replace the src of the HTML element
+//     img.onload = () => {
+//       item.classList.remove('asyncImage');
+//       return item.nodeName === 'IMG' ? 
+//         item.src = item.dataset.src :        
+//         item.style.backgroundImage = `url(${item.dataset.src})`;
+//     };
+//   });
+// })();
 
 // TODO: Figure out why natural width is sometimes 0
 function handleClick() {
